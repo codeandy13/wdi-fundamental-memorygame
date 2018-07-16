d97aea8ff0b1f414665ae85e5da4e423aa710f8a
codeandy13 <37026377+codeandy13@users.noreply.github.com>
2018-07-16 12:56:34 -0400

    Add files via upload

---
+++ Fundamentals/memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "quenn";
+var cardTwo = "queen";
+var cardThree = "King";
+var cardFour = "King";
+
+console.log ("user flipped " + cardOne);
+console.log ("user flipped " + cardThree);
