ea5e0467106e74a312976065527631a920068e82
codeandy13 <37026377+codeandy13@users.noreply.github.com>
2018-07-22 22:37:45 -0400

    Add files via upload

--- Fundamentals/memory_game/js/main.js
+++ Fundamentals/memory_game/js/main.js
@@ -1,10 +1,22 @@
-console.log("Up and running!");
 
-var cardOne = "quenn";
-var cardTwo = "queen";
-var cardThree = "King";
-var cardFour = "King";
 
-console.log ("user flipped " + cardOne);
-console.log ("user flipped " + cardThree);
-console.log ("user flipped " + cardTwo);
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+
+
+
+var cardOne = [0];
+cardsInPlay.push('cardOne');
+console.log("user flipped queen");
+
+var cardTwo = [2];
+cardsInPlay.push('cardTwo');
+console.log("user flipped king");
+
+if (cardsInPlay === 2){
+cardsInPlay.length = 2;
+} else if (cardsInPlay[0] === cardsInPlay[1]){
+	alert ("You found a match!");
+} else (){
+	alert ("Sorry, try again.");
+}
